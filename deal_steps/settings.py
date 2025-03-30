54cfdc33c8c84e0e17d9333d87c95a507b9e8b74
tarek339 <tarekjassine@gmail.com>
2025-03-30 03:19:42 +0200

    update database settings for PostgreSQL and add psycopg2-binary to dependencies; update .gitignore to include additional data files

--- deal_steps/settings.py
+++ deal_steps/settings.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = "deal_steps.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": env("POSTGRE_ENGINE"),
+        "NAME": "postgres",
+        "USER": env("POSTGRE_USER"),
+        "PASSWORD": env("POSTGRE_PASSWORD"),
+        "HOST": env("POSTGRE_HOST"),  # oder die Adresse Ihres PostgreSQL-Servers
+        "PORT": env("POSTGRE_PORT"),  # Standard-PostgreSQL-Port
     }
 }
 
